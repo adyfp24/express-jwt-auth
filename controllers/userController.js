d40fa15ca3d2da467869d9002fb0dab8f2071730
adyfp24 <adyfp24@gmail.com>
2024-01-22 17:38:38 +0700

    feat(endpoint me): adding getMe method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,28 @@
-// const User = require('../models').User;
+const User = require('../models').User;
 
-const me = async (req, res) => {
+const getMe = (req, res) => {
     try {
+        const user = req.user;
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Anda tidak terautentikasi',
+            });
+        }
+
         res.status(200).json({
             success: true,
-            message: 'anda telah terautentikasi login',
-            data: user
-        })
+            message: 'Anda telah terautentikasi login',
+            data: user,
+        });
     } catch (error) {
-        res.status(error).json({
+        console.error(error);
+        res.status(500).json({
             success: false,
-            message: 'anda tidak terautentikasi',
-        })
+            message: 'Terjadi kesalahan dalam server',
+        });
     }
 }
 
-module.exports = {me}
+module.exports = { getMe }
